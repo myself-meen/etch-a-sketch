82a86beb0cec7bddc2bf6fc1bda6947c1f195247
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2025-09-29 12:06:49 +0000

    Add basic functionality (static resizing)

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-goit
+let gridcount=16;
+const container=document.getElementById("container")
+creategrid(gridcount);
+function creategrid(size){
+for(let a=1;a<=size*size;a++){
+let square=document.createElement("div")
+square.className="square"
+
+container.appendChild(square);
+square.addEventListener("mouseover", () => {
+  square.style.backgroundColor = "black";
+});
+}
+}
+let square=document.querySelectorAll(".square")
+const btn=document.getElementById("reset-btn")
+btn.addEventListener("click",()=>{
+    container.innerHTML="";
+   let input= prompt("Enter grid size: (1-100)")
+   let nsize=parseInt(input);
+   if(nsize>0&&nsize<=100){
+    creategrid(nsize)
+   }
+   else{
+alert("please enter a valid number between 1-100")
+   }
+})
